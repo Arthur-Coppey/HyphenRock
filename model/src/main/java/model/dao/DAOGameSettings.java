cb2ff99dc361e8f960903b48403e26c35fc5b14f
Arthur-Coppey <arthur.coppey@viacesi.fr>
2019-05-27 16:51:06 +0200

    added DAOGameSettings

---
+++ model/src/main/java/model/dao/DAOGameSettings.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package model.dao;
+
+/**
+ * @author Arthur Coppey
+ *
+ */
+class DAOGameSettings {
+    
+    /**
+     * 
+     */
+    public DAOGameSettings() {
+        // TODO Auto-generated constructor stub
+    }
+    
+}
