645b264d0ce31290c533472fba5f57b555a19a8a
Arthur-Coppey <arthur.coppey@viacesi.fr>
2019-05-27 14:16:30 +0200

    fix bug

--- controller/src/main/java/controller/Controller.java
+++ controller/src/main/java/controller/Controller.java
@@ -9,86 +9,84 @@ import contract.IView;
  * The Class Controller.
  */
 public final class Controller implements IController {
-
-	/** The view. */
-	private IView		view;
-
-	/** The model. */
-	private IModel	model;
-
-	/**
-	 * Instantiates a new controller.
-	 *
-	 * @param view
-	 *          the view
-	 * @param model
-	 *          the model
-	 */
-	public Controller(final IView view, final IModel model) {
-		this.setView(view);
-		this.setModel(model);
-	}
-
-	/**
+    
+    /** The model. */
+    private IModel model;
+    
+    /** The view. */
+    private IView view;
+    
+    /**
+     * Instantiates a new controller.
+     *
+     * @param view
+     *              the view
+     * @param model
+     *              the model
+     */
+    public Controller(final IView view, final IModel model) {
+        this.setView(view);
+        this.setModel(model);
+    }
+    
+    /**
      * Control.
      */
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see contract.IController#control()
-	 */
-	public void control() {
-		this.view.printMessage("Appuyer sur les touches 'E', 'F', 'D' ou 'I', pour afficher Hello world dans la langue d votre choix.");
-	}
-
-	/**
-     * Sets the view.
-     *
-     * @param pview
-     *            the new view
+    /*
+     * (non-Javadoc)
+     * @see contract.IController#control()
      */
-	private void setView(final IView pview) {
-		this.view = pview;
-	}
-
-	/**
-	 * Sets the model.
-	 *
-	 * @param model
-	 *          the new model
-	 */
-	private void setModel(final IModel model) {
-		this.model = model;
-	}
-
-	/**
+    @Override
+    public void control() {
+        this.view.printMessage(
+            "Appuyer sur les touches 'E', 'F', 'D' ou 'I', pour afficher Hello world dans la langue d votre choix."
+        );
+    }
+    
+    /**
      * Order perform.
      *
      * @param controllerOrder
-     *            the controller order
+     *                        the controller order
      */
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see contract.IController#orderPerform(contract.ControllerOrder)
-	 */
-	public void orderPerform(final ControllerOrder controllerOrder) {
-		switch (controllerOrder) {
-			case English:
-				this.model.loadHelloWorld("GB");
-				break;
-			case Francais:
-				this.model.loadHelloWorld("FR");
-				break;
-			case Deutsch:
-				this.model.loadHelloWorld("DE");
-				break;
-			case Indonesia:
-				this.model.loadHelloWorld("ID");
-				break;
-			default:
-				break;
-		}
-	}
-
+    /*
+     * (non-Javadoc)
+     * @see contract.IController#orderPerform(contract.ControllerOrder)
+     */
+    @Override
+    public void orderPerform(final ControllerOrder controllerOrder) {
+        switch (controllerOrder) {
+            case English:
+                break;
+            case Francais:
+                break;
+            case Deutsch:
+                break;
+            case Indonesia:
+                break;
+            default:
+                break;
+        }
+    }
+    
+    /**
+     * Sets the model.
+     *
+     * @param model
+     *              the new model
+     */
+    private void setModel(final IModel model) {
+        this.model = model;
+    }
+    
+    /**
+     * Sets the view.
+     *
+     * @param pview
+     *              the new view
+     */
+    private void setView(final IView pview) {
+        this.view = pview;
+    }
+    
 }
