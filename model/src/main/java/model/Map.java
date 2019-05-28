6bcc19b45910e117b009231bbcfb87433698aeab
Arthur-Coppey <arthur.coppey@viacesi.fr>
2019-05-28 09:40:04 +0200

    renamed class MapMaker to Map

---
+++ model/src/main/java/model/Map.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import model.element.Element;
+
+public class Map {
+	private Element[][] mapping;
+	private ArrayList<Element> elements;
+	private HashMap<String, Integer> score;
+	
+}
