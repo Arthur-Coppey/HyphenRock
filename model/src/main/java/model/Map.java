a79f0ebb89807dc51da95a74b5df375eddf6f924
Arthur-Coppey <arthur.coppey@viacesi.fr>
2019-05-28 10:43:28 +0200

    finished Map class base features

--- model/src/main/java/model/Map.java
+++ model/src/main/java/model/Map.java
@@ -6,8 +6,44 @@ import java.util.HashMap;
 import model.element.Element;
 
 public class Map {
-	private Element[][] mapping;
-	private ArrayList<Element> elements;
-	private HashMap<String, Integer> score;
-	
+    private ArrayList<Element>       elements;
+    private final Element[][]        mapping;
+    private String                   name;
+    private HashMap<String, Integer> score;
+    private final int                width, height;
+    
+    Map(final String name, final int width, final int height) {
+        this.name    = name;
+        this.width   = width;
+        this.height  = height;
+        this.mapping = new Element[width][height];
+    }
+
+    Element getElementByPosition(final int x, final int y) throws Exception {
+        return this.mapping[x][y];
+    }
+
+    void placeElement(final Element element) {
+        final int x = element.getX();
+        final int y = element.getY();
+        if (this.getElementByPosition(x, y) == null) {
+            this.setElementToPosition(element, x, y);
+        }
+    }
+    
+    void setElements(final ArrayList<Element> elements) {
+        this.elements = elements;
+    }
+    
+    void setElementToPosition(final Element element, final int x, final int y) throws Exception {
+        this.mapping[x][y] = element;
+    }
+    
+    void setName(final String name) {
+        this.name = name;
+    }
+
+    void setScore(final HashMap<String, Integer> score) {
+        this.score = score;
+    }
 }
