28aa58b50def5fdcf1061937ff9799bd77e77b7c
Eli0ttD0natien <eliott.donatien@viacesi.fr>
2019-05-28 10:15:32 +0200

    Starting updating mobile elements

--- model/src/main/java/model/Model.java
+++ model/src/main/java/model/Model.java
@@ -2,66 +2,60 @@ package model;
 
 import java.util.Observable;
 
+import contract.Direction;
 import contract.IModel;
 import model.element.Element;
 
-
-/**
- * The Class Model.
- *
- * @author Jean-Aymeric Diet
- */
 public final class Model extends Observable implements IModel {
 
+    /** The helloWorld. */
+    private Element[][]    map;
+    private int            score;
+    private final MapMaker mapMaker;
+    private final String[] elementTab = { "player", "Mob", "Rock", "Diamond" };
 
-	/** The helloWorld. */
-	private Element[][] map;
-	private int score;
-	private final MapMaker mapMaker;
+    /**
+     * Instantiates a new model.
+     */
+    public Model() {
 
-	/**
-	 * Instantiates a new model.
-	 */
-	public Model() {
+        this.mapMaker = new MapMaker();
+    }
 
-		this.mapMaker = new MapMaker();
-	}
+    @Override
+    public void gameUpdate(Direction direction) {
 
-	public Element[][] getMap(int level) {
-		return this.map;
-	}
+    }
 
-	public void setMap(Element[][] map) {
-		this.map = map;
-	}
+    public Element[][] getMap(int level) {
+        return this.map;
+    }
 
-	/**
-	 * Gets the observable.
-	 *
-	 * @return the observable
-	 */
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see contract.IModel#getObservable()
-	 */
-	@Override
-	public Observable getObservable() {
-		return this;
-	}
+    /**
+     * Gets the observable.
+     *
+     * @return the observable
+     */
+    /*
+     * (non-Javadoc)
+     *
+     * @see contract.IModel#getObservable()
+     */
+    @Override
+    public Observable getObservable() {
+        return this;
+    }
 
-	public int getScore() {
-		return this.score;
-	}
+    public int getScore() {
+        return this.score;
+    }
 
-	public void setScore(int score) {
-		this.score = score;
-	}
+    public void setMap(Element[][] map) {
+        this.map = map;
+    }
 
-	@Override
-	public void gameUpdate(String code) {
-		// TODO Auto-generated method stub
-
-	}
+    public void setScore(int score) {
+        this.score = score;
+    }
 
 }
--- model/src/main/java/model/element/Mobile.java
+++ model/src/main/java/model/element/Mobile.java
@@ -4,9 +4,37 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public abstract class Mobile extends Element {
+    private int     x;
+    private int     y;
+    private boolean falling = false;
 
     public Mobile(BufferedImage Sprite) throws IOException {
         super(Sprite);
         // TODO Auto-generated constructor stub
     }
+
+    public void fall(boolean i) {
+        this.falling = i;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public boolean isFalling() {
+        return this.falling;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
 }
