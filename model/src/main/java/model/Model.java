04578f260095a6cb60607b2df70e53ee65632a56
Arthur-Coppey <arthur.coppey@viacesi.fr>
2019-06-03 20:33:16 +0200

    uncommented a line

--- model/src/main/java/model/Model.java
+++ model/src/main/java/model/Model.java
@@ -10,22 +10,22 @@ import model.dao.GameSettingsProperties;
 import model.element.Element;
 
 public final class Model extends Observable implements IModel {
-    
+
     private final DAOMap                 daoMap;
     private final GameSettingsProperties gameSettings = new GameSettingsProperties();
     private Map                          map;
     private Element[][]                  mapping;
     private int                          score;
-    
+
     public Model() {
         this.daoMap = new DAOMap(DBConnection.getInstance().getConnection());
-        // this.map = this.daoMap.loadMap(this.gameSettings.getMapId());
+        this.map    = this.daoMap.loadMap(this.gameSettings.getMapId());
     }
-    
+
     public Map createMapFromFile(final String fileName) {
         return this.daoMap.createMapFromFile(fileName);
     }
-    
+
     @Override
     public void gameUpdate(final Direction direction) throws Exception {
         this.map.getPlayer().playerUpdate(direction, this.map);
@@ -33,38 +33,38 @@ public final class Model extends Observable implements IModel {
             element.update();
         }
     }
-    
+
     public Map getMap() {
         return this.map;
     }
-
+    
     public Element[][] getMapping(final int level) {
         return this.mapping;
     }
-    
+
     @Override
     public Observable getObservable() {
         return this;
     }
-    
+
     public int getScore() {
         return this.score;
     }
-    
+
     public void saveMap() {
         this.daoMap.saveMap(this.map);
     }
-    
+
     public void setMap(final Map map) {
         this.map = map;
     }
-    
+
     public void setMapping(final Element[][] mapping) {
         this.mapping = mapping;
     }
-    
+
     public void setScore(final int score) {
         this.score = score;
     }
-    
+
 }
