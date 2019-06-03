f5d8323658008d8fd3cc72ad4dad443afbc41137
Arthur-Coppey <arthur.coppey@viacesi.fr>
2019-06-03 21:28:56 +0200

    removed useless comments

--- view/src/main/java/view/ViewFrame.java
+++ view/src/main/java/view/ViewFrame.java
@@ -1,11 +1,10 @@
-
 package view;
-
 
 import java.awt.HeadlessException;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.IOException;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,107 +13,74 @@ import javax.swing.JOptionPane;
 import contract.IController;
 import contract.IModel;
 
-class ViewFrame extends JFrame  implements KeyListener {
+class ViewFrame extends JFrame implements KeyListener {
 
-	private IModel model;
-	private ImageIcon icon = new ImageIcon("");
-	private JLabel image = new JLabel(icon);
-	
-	private IController controller;
-	
-	private static final long serialVersionUID = -697358409737458175L;
-	//private static final int FRAMEHEIGHT = 500;
-	//private static final int FRAMEWIDTH = 500;
-	private Camera camera;
-	private JFrame frame = new JFrame("HyphenRock");
-	
-	//private final File groundImageFile = new File("C:\\ground.png");
-	//private final File wallImageFile = new File("C:\\wall.png");
-	private int cellWidth = 32;
-	private int cellHeight = 32;
-	//private final BufferedImage img;
-	//private final BufferedImage groundImage;
-	//private final BufferedImage wallImage;
-	//private final Graphics g;
-	
-	public ViewFrame(final IModel model) throws HeadlessException, IOException {
-		this.camera = new Camera();
-		//this.img = new BufferedImage(this.camera.getWIDTH() * cellWidth, this.camera.getHEIGHT() * cellHeight, BufferedImage.TYPE_INT_ARGB);
-		//this.g= img.getGraphics();
-//
-		//this.groundImage = ImageIO.read(groundImageFile);
-		//this.wallImage = ImageIO.read(wallImageFile);
-		//this.backgroundMapMaking(this.groundImage, this.wallImage);
-		
+    private static final int  cellHeight       = 16;
+    private static final int  cellWidth        = 16;
+    private static final long serialVersionUID = -697358409737458175L;
 
-	}
+    private final Camera camera;
 
-//	public ViewFrame(final IModel model, final GraphicsConfiguration gc) {
-//		super(gc);
-//		this.buildViewFrame(model);
-//		groundImage = ImageIO.read(groundImageFile);
-//	}
-//
-//	public ViewFrame(final IModel model, final String title) throws HeadlessException {
-//		super(title);
-//		this.buildViewFrame(model);
-//		groundImage = ImageIO.read(groundImageFile);
-//	}
-//
-//	public ViewFrame(final IModel model, final String title, final GraphicsConfiguration gc) {
-//		super(title, gc);
-//		this.buildViewFrame(model);
-//		groundImage = ImageIO.read(groundImageFile);
-//	}
+    private IController     controller;
+    private final JFrame    frame = new JFrame("HyphenRock");
+    private final ImageIcon icon  = new ImageIcon("");
 
-	private IController getController() {
-		return this.controller;
-	}
+    private final JLabel image = new JLabel(this.icon);
+    private IModel       model;
 
-	protected void setController(final IController controller) {
-		this.controller = controller;
-	}
+    public ViewFrame(final IModel model) throws HeadlessException, IOException {
+        this.camera = new Camera();
+    }
 
-	protected IModel getModel() {
-		return this.model;
-	}
+    @Override
+    public void keyPressed(final KeyEvent e) {
+        this.getController().orderPerform(View.keyCodeToControllerOrder(e.getKeyCode()));
+        View.keyCodeToControllerOrder(e.getKeyCode());
+    }
 
-	private void setModel(final IModel model) {
-		this.model = model;
-	}
+    @Override
+    public void keyReleased(final KeyEvent e) {
+        
+    }
 
-	private void buildViewFrame(final IModel model) throws IOException {
-		this.setModel(model);
-		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.frame.setResizable(false);
-		this.frame.addKeyListener(this);
-		this.frame.setSize(this.camera.getWIDTH()*cellWidth + this.getInsets().left + this.getInsets().right,
-		this.camera.getHEIGHT()*cellHeight + this.getInsets().top + this.getInsets().bottom);
-		this.frame.setContentPane(new ViewPanel(this));
-		this.frame.setLocationRelativeTo(null);
-		this.frame.add(image);
-		this.frame.setVisible(true);
-		
-	}
+    @Override
+    public void keyTyped(final KeyEvent e) {
 
-	public void printMessage(final String message) {
-		JOptionPane.showMessageDialog(null, message);
-	}
+    }
 
-	@Override
-	public void keyTyped(final KeyEvent e) {
+    public void printMessage(final String message) {
+        JOptionPane.showMessageDialog(null, message);
+    }
 
-	}
+    protected IModel getModel() {
+        return this.model;
+    }
 
-	@Override
-	public void keyPressed(final KeyEvent e) {
-		this.getController().orderPerform(View.keyCodeToControllerOrder(e.getKeyCode()));
-		View.keyCodeToControllerOrder(e.getKeyCode());
-	}
+    protected void setController(final IController controller) {
+        this.controller = controller;
+    }
 
-	@Override
-	public void keyReleased(final KeyEvent e) {
+    private void buildViewFrame(final IModel model) throws IOException {
+        this.setModel(model);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setResizable(false);
+        this.frame.addKeyListener(this);
+        this.frame.setSize(
+            (this.camera.getWIDTH() * ViewFrame.cellWidth) + this.getInsets().left + this.getInsets().right,
+            (this.camera.getHEIGHT() * ViewFrame.cellHeight) + this.getInsets().top + this.getInsets().bottom
+        );
+        this.frame.setContentPane(new ViewPanel(this));
+        this.frame.setLocationRelativeTo(null);
+        this.frame.add(this.image);
+        this.frame.setVisible(true);
 
-	}
+    }
+
+    private IController getController() {
+        return this.controller;
+    }
+
+    private void setModel(final IModel model) {
+        this.model = model;
+    }
 }
-
