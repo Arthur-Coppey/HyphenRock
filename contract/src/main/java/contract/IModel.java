c337fd6dcefad4cfba08b3679a4e151c02533142
Arthur-Coppey <arthur.coppey@viacesi.fr>
2019-06-03 22:04:41 +0200

    added getSprites method so the view can get them

--- contract/src/main/java/contract/IModel.java
+++ contract/src/main/java/contract/IModel.java
@@ -4,6 +4,10 @@ import java.util.Observable;
 
 public interface IModel {
     void gameUpdate(Direction direction) throws Exception;
-
+    
     Observable getObservable();
+    
+    /**
+     *
+     */
 }
--- model/src/main/java/model/Model.java
+++ model/src/main/java/model/Model.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.awt.image.BufferedImage;
 import java.util.Observable;
 
 import contract.Direction;
@@ -10,22 +11,22 @@ import model.dao.GameSettingsProperties;
 import model.element.Element;
 
 public final class Model extends Observable implements IModel {
-
+    
     private final DAOMap                 daoMap;
     private final GameSettingsProperties gameSettings = new GameSettingsProperties();
     private Map                          map;
     private Element[][]                  mapping;
     private int                          score;
-
+    
     public Model() {
         this.daoMap = new DAOMap(DBConnection.getInstance().getConnection());
         this.map    = this.daoMap.loadMap(this.gameSettings.getMapId());
     }
-
+    
     public Map createMapFromFile(final String fileName) {
         return this.daoMap.createMapFromFile(fileName);
     }
-
+    
     @Override
     public void gameUpdate(final Direction direction) throws Exception {
         this.map.getPlayer().playerUpdate(direction, this.map);
@@ -33,38 +34,50 @@ public final class Model extends Observable implements IModel {
             element.update();
         }
     }
-
+    
     public Map getMap() {
         return this.map;
     }
-    
-    public Element[][] getMapping(final int level) {
+
+    public Element[][] getMapping() {
         return this.mapping;
     }
-
+    
     @Override
     public Observable getObservable() {
         return this;
     }
-
+    
     public int getScore() {
         return this.score;
     }
-
+    
+    public BufferedImage[][] getSprites() {
+        final BufferedImage[][] sprites;
+        final Element[][]       elements = this.getMapping();
+        sprites = new BufferedImage[this.map.getWidth()][this.map.getHeight()];
+        for (int i = 0; i < sprites.length; i++ ) {
+            for (int j = 0; j < sprites[i].length; j++ ) {
+                sprites[i][j] = elements[i][j].getSprite();
+            }
+        }
+        return sprites;
+    }
+    
     public void saveMap() {
         this.daoMap.saveMap(this.map);
     }
-
+    
     public void setMap(final Map map) {
         this.map = map;
     }
-
+    
     public void setMapping(final Element[][] mapping) {
         this.mapping = mapping;
     }
-
+    
     public void setScore(final int score) {
         this.score = score;
     }
-
+    
 }
