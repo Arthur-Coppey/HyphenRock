cb969d231905a0724cb5e931f9c2054db76a1e0f
Arthur-Coppey <arthur.coppey@viacesi.fr>
2019-06-03 21:11:26 +0200

    enforced lower case characters on method first character

--- model/src/main/java/model/element/Mobile.java
+++ model/src/main/java/model/element/Mobile.java
@@ -10,43 +10,43 @@ public abstract class Mobile extends Element {
     protected int   x;
     protected int   y;
     private boolean falling = false;
-
+    
     public Mobile(final BufferedImage Sprite) throws IOException {
         super(Sprite);
     }
-
-    public int DirectionToInt(final Direction direction) {
+    
+    public int directionToInt(final Direction direction) {
         return (Direction.valueOf(direction.toString()).ordinal());
     }
-
+    
     public int getX() {
         return this.x;
     }
-
+    
     public int getY() {
         return this.y;
     }
-
-    public Direction IntToDirection(final int direction) {
+    
+    public Direction intToDirection(final int direction) {
         return Direction.values()[direction];
     }
-
+    
     public boolean isFalling() {
         return this.falling;
     }
-
+    
     public void setFalling(final boolean i) {
         this.falling = i;
     }
-
+    
     public void setX(final int x) {
         this.x = x;
     }
-
+    
     public void setY(final int y) {
         this.y = y;
     }
-
+    
     /**
      * @param map
      *            the map on which the element is
@@ -55,9 +55,9 @@ public abstract class Mobile extends Element {
      */
     public void update(final Map map) throws Exception {
         // TODO Auto-generated method stub
-        
-    }
 
+    }
+    
     protected int[] directionCoordinate(final int direction) {
         final int[][] tabDirection = {
             {
@@ -72,7 +72,7 @@ public abstract class Mobile extends Element {
         };
         return tabDirection[direction];
     }
-
+    
     protected void explode(final Map map) throws Exception {
         int x;
         int y;
@@ -83,31 +83,31 @@ public abstract class Mobile extends Element {
             map.getElements().remove(map.getElementByPosition(x, y));
         }
     }
-
+    
     protected Element getEast(final Map map, final int x, final int y) throws Exception {
         return map.getElementByPosition(x + 1, y);
     }
-
+    
     protected Element getInDirection(final Direction direction, final Map map) throws Exception {
         return map.getElementByPosition(
-            this.directionCoordinate(this.DirectionToInt(direction))[0] + this.getX(), this.directionCoordinate(this.DirectionToInt(direction))[1] + this.getY()
+            this.directionCoordinate(this.directionToInt(direction))[0] + this.getX(), this.directionCoordinate(this.directionToInt(direction))[1] + this.getY()
         );
     }
-
+    
     protected Element getSouth(final Map map, final int x, final int y) throws Exception {
         return map.getElementByPosition(x, y + 1);
     }
-
+    
     protected Element getSouthEast(final Map map, final int x, final int y) throws Exception {
         return map.getElementByPosition(x + 1, y - 1);
     }
-
+    
     protected Element getSouthWest(final Map map, final int x, final int y) throws Exception {
         return map.getElementByPosition(x - 1, y - 1);
     }
-
+    
     protected Element getWest(final Map map, final int x, final int y) throws Exception {
         return map.getElementByPosition(x - 1, y);
     }
-
+    
 }
