69e85290e8edd72eee3f0281f6c4ecb07d049386
Arthur-Coppey <arthur.coppey@viacesi.fr>
2019-06-03 21:13:50 +0200

    attribute replaced by local variable

--- controller/src/main/java/controller/Controller.java
+++ controller/src/main/java/controller/Controller.java
@@ -8,50 +8,48 @@ import contract.IModel;
 import contract.IView;
 
 public final class Controller implements IController {
-    
+
     private final int FPS = 1;
-    
+
     private Loop loop;
-    
+
     private IModel model;
-    
-    private Timer timer;
-    
+
     private IView view;
-    
+
     public Controller(final IView view, final IModel model) {
         this.setView(view);
         this.setModel(model);
         this.setLoop(new Loop(this.model));
     }
-    
+
     @Override
     public void control() {
         this.view.printMessage("Jouez avec ZQSD");
     }
-    
+
     public void gameStart() {
-        this.timer = new Timer();
-        this.timer.schedule(this.getLoop(), 0, 1000 / this.FPS);
+        final Timer timer = new Timer();
+        timer.schedule(this.getLoop(), 0, 1000 / this.FPS);
     }
-    
+
     public Loop getLoop() {
         return this.loop;
     }
-    
+
     @Override
     public void orderPerform(final Direction direction) {
-        
+
         if (this.getLoop().getDirectionOrder() == null) {
             System.out.println(direction);
             this.getLoop().setDirectionOrder(direction);
         }
     }
-    
+
     public void setLoop(final Loop loop) {
         this.loop = loop;
     }
-    
+
     /**
      * Sets the model.
      *
@@ -61,7 +59,7 @@ public final class Controller implements IController {
     private void setModel(final IModel model) {
         this.model = model;
     }
-    
+
     /**
      * Sets the view.
      *
@@ -71,5 +69,5 @@ public final class Controller implements IController {
     private void setView(final IView pview) {
         this.view = pview;
     }
-    
+
 }
