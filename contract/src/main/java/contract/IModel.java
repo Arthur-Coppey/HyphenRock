dffac6b0e0bf50b0a28e97a21edabf84adf5a129
Arthur-Coppey <arthur.coppey@viacesi.fr>
2019-06-03 22:11:56 +0200

    forgot this

--- contract/src/main/java/contract/IModel.java
+++ contract/src/main/java/contract/IModel.java
@@ -1,13 +1,15 @@
 package contract;
 
+import java.awt.image.BufferedImage;
 import java.util.Observable;
 
 public interface IModel {
     void gameUpdate(Direction direction) throws Exception;
-    
+
     Observable getObservable();
-    
+
     /**
      *
      */
+    BufferedImage[][] getSprites();
 }
